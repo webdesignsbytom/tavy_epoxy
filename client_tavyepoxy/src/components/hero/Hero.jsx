9b22487dc803417993345f4ae59f6f005aedb502
Thomas Brockington <xtombrock1989@gmail.com>
2023-01-08 01:24:49 +0000

    nav and hero updates

--- client_tavyepoxy/src/components/hero/Hero.jsx
+++ client_tavyepoxy/src/components/hero/Hero.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import './hero.css';
-
+import './hero.css'
 function Hero() {
   return (
     <>
-      <div className='titles__container'>
+      <div className='hero__container'>
+        <div className="hero__titles__container">
           <h1 className='main__title'>TAV-E-POXY</h1>
-          <h2 className='subtitle'>Unique furnature hand-made to order</h2>
+          <h2 className='subtitle'>Unique epoxy furnature hand-made to order</h2>
         </div>
+      </div>
     </>
   );
 }
--- client_tavyepoxy/src/components/slider/Slider.jsx
+++ client_tavyepoxy/src/components/slider/Slider.jsx
@@ -5,7 +5,7 @@ import CoffeeTable1 from '../../assets/images/coffee_table.jpg'
 function Slider() {
   return (
     <>
-      <div className='hero__container'>
+      <div className='slider__container'>
         <div className="image__slider__container">
             <div className="image__container">
               <img src={CoffeeTable1} alt="Coffee Table wood striation design" />

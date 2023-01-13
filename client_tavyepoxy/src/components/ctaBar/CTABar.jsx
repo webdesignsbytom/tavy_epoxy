fb1b5d7ba229f38b447d4f889b11895261e57c5b
Thomas Brockington <xtombrock1989@gmail.com>
2023-01-13 20:11:38 +0000

    safety save

--- client_tavyepoxy/src/components/ctaBar/CTABar.jsx
+++ client_tavyepoxy/src/components/ctaBar/CTABar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './ctabar.css';
 
 function CTABar() {
   return (
-    <section className='cta__container'>
+    <section className='cta__container' id='cta'>
       <div className='button__container'>
         <button className='enquire__button cta__button'>Enquire</button>
       </div>
--- client_tavyepoxy/src/components/hero/Hero.jsx
+++ client_tavyepoxy/src/components/hero/Hero.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
-import './hero.css'
+import { useState } from 'react';
+import './hero.css';
 function Hero() {
+  const [clickedContinue, setClickedContinue] = useState(null)
+
+  console.log('clickedContinue', clickedContinue);
+
+  const scrollDown = ()=> {
+    window.scrollBy(0, 90)
+    console.log(window.screen.availHeight);
+console.log('window.screen.availWidth', window.screen.availWidth);
+  }
   return (
     <>
       <div className='hero__container'>
-        <div className="hero__titles__container">
+        <div className='hero__titles__container'>
           <h1 className='main__title'>TAV-E-POXY</h1>
-          <h2 className='subtitle'>Unique epoxy furnature hand-made to order</h2>
+          <h2 className='subtitle'>
+            Unique epoxy furniture hand-made to order
+          </h2>
         </div>
+        <div className='continue__btn__container' >
+          <button className='continue__btn' id='continue__btn'>
+          <a href='#cta' onClick={() => setClickedContinue('#cta')} className={clickedContinue === '#cta' ? 'active' : ''}>CONTINUE</a>
+          </button>
+        </div>
+
       </div>
     </>
   );

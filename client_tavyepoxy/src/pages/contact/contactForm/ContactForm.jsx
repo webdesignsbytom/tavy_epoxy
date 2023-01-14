245705460f379d2eac1eb5d5d55afc70ecee1b67
Thomas Brockington <xtombrock1989@gmail.com>
2023-01-14 05:05:06 +0000

    contact init

---
+++ client_tavyepoxy/src/pages/contact/contactForm/ContactForm.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './contactForm.css';
+
+function ContactForm() {
+  return (
+    <div className='contact__container'>
+
+      <main className='contact__form'>
+
+        <div className='order__options'>
+            <div className="table__sq">Table</div>
+            <div className="table__set">Table</div>
+            <div className="art__piece">Table</div>
+        </div>
+
+        <div className='form__container'>
+            <form action="" className='contact__form__actions'>
+                hello
+            </form>
+        </div>
+      </main>
+      
+    </div>
+  );
+}
+
+export default ContactForm;

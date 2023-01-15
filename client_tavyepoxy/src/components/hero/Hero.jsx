c6259aa9be47e719496890a3b69bc162e29006ba
Thomas Brockington <xtombrock1989@gmail.com>
2023-01-15 23:32:40 +0000

    safet save

--- client_tavyepoxy/src/components/hero/Hero.jsx
+++ client_tavyepoxy/src/components/hero/Hero.jsx
@@ -2,24 +2,32 @@ import React from 'react';
 import { useState } from 'react';
 import './hero.css';
 function Hero() {
-  const [clickedContinue, setClickedContinue] = useState(null)
-
+  const [clickedContinue, setClickedContinue] = useState(null);
 
   return (
+    <>
       <div className='hero__container'>
         <div className='hero__titles__container'>
-          <h1 className='main__title'>TAV-E-<span className='second__font'>POXY</span></h1>
+          <h1 className='main__title'>
+            TAV-E-<span className='second__font'>POXY</span>
+          </h1>
           <h2 className='subtitle'>
             Hand-made furniture with a unqiue and stunning epoxy finish
           </h2>
         </div>
-        <div className='continue__btn__container' >
+        <div className='continue__btn__container'>
           <button className='continue__btn' id='continue__btn'>
-          <a href='#cta' onClick={() => setClickedContinue('#cta')} className={clickedContinue === '#cta' ? 'active' : ''}>Continue</a>
+            <a
+              href='#cta'
+              onClick={() => setClickedContinue('#cta')}
+              className={clickedContinue === '#cta' ? 'active' : ''}
+            >
+              Continue
+            </a>
           </button>
         </div>
-
       </div>
+    </>
   );
 }
 

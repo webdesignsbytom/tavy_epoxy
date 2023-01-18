8dde135e90873f0608691a19c1e257ea852357a7
Thomas Brockington <xtombrock1989@gmail.com>
2023-01-18 02:32:09 +0000

    minor

--- client_tavyepoxy/src/pages/contact/contactForm/ContactForm.jsx
+++ client_tavyepoxy/src/pages/contact/contactForm/ContactForm.jsx
@@ -3,10 +3,10 @@ import { useState } from 'react';
 import './contactForm.css';
 
 function ContactForm() {
-  const [formData, setFormData] = useState({})
+  const [formData, setFormData] = useState({});
 
   const handleChange = (event) => {
-console.log('event', event.target.id);
+    console.log('event', event.target.id);
     const { value, name } = event.target;
     console.log('value', value, 'name', name);
     console.log('formData', formData);
@@ -22,14 +22,20 @@ console.log('event', event.target.id);
     <div className='contact__container'>
       <main className='contact__form'>
         <div className='order__options'>
-          <div className='table__sq' value='table' id='table' onClick={handleChange}>Table</div>
+          <div
+            className='table__sq'
+            value='table'
+            id='table'
+            onClick={handleChange}
+          >
+            Table
+          </div>
           <div className='table__set'>Dining Set</div>
           <div className='art__piece'>ArtWork</div>
         </div>
 
         <div className='form__container'>
           <form onSubmit={handleSubmit} className='contact__form__actions'>
-
             <div className='email__container'>
               <label htmlFor='email'>Email:</label>
               <input

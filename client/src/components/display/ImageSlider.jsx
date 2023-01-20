02097fde0648520e97edd687d10a2f62af59ed7a
xtombrock <xtombrock1989@gmail.com>
2023-01-20 18:35:30 +0000

    slider working linear

--- client/src/components/display/ImageSlider.jsx
+++ client/src/components/display/ImageSlider.jsx
@@ -1,24 +1,39 @@
-import React from 'react'
-import NorthernLights from '../../assets/images/northern_lights_crop.png';
-
+import React, { useEffect, useState } from 'react';
+import { sliderOptions } from '../../utils/ImageLocations';
 
 function ImageSlider() {
+  const startingCategory = sliderOptions[0];
+
+  const [currentImage, setCurrentImage] = useState(sliderOptions);
+  const [indexNumber, setIndexNumber] = useState(0);
+
+  useEffect(() => {
+    console.log('effect');
+    const interval = setInterval(() => {
+        setIndexNumber((prev) => prev + 1);
+      
+    }, 6000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <section className='slider__container'>
+      <div className='current__image'>
+        <img src={currentImage[indexNumber]} alt='norther lights display' />
+      </div>
 
-          <div className='current__image'>
-            <img src={NorthernLights} alt='norther lights display' />
-          </div>
-
-          <div className='slider__control'>
-            <div className="slider__icon"></div>
-            <div className="slider__icon"></div>
-            <div className="slider__icon"></div>
-            <div className="slider__icon"></div>
-            <div className="slider__icon"></div>
-          </div>
-        </section>
-  )
+      <div className='slider__control'>
+        <div className='slider__icon'></div>
+        <div className='slider__icon'></div>
+        <div className='slider__icon'></div>
+        <div className='slider__icon'></div>
+        <div className='slider__icon'></div>
+      </div>
+    </section>
+  );
 }
 
-export default ImageSlider
+export default ImageSlider;

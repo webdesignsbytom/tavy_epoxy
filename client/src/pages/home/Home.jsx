76d8b0263ae6bc17e54c0cc583128542ec9b1b33
xtombrock <xtombrock1989@gmail.com>
2023-01-20 04:43:26 +0000

    options

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Nav from '../../components/nav/Nav';
 import Hero from '../../components/hero/Hero';
 import Display from '../../components/display/Display';
+import Options from '../../components/options/Options';
 
 
 function Home() {
@@ -10,6 +11,7 @@ function Home() {
     <Nav />
         <Hero />
         <Display />
+        <Options />
     </>
   );
 }

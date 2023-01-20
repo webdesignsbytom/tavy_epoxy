0190b1c66f4728e3867a96af87f318d8801160a7
xtombrock <xtombrock1989@gmail.com>
2023-01-20 07:29:12 +0000

    footer and register

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
-import './footer.css'
+import './footer.css';
 
 function Footer() {
   return (
     <>
       <footer className='footer__container'>
-        <div className="contact__information">
-            Contact info
+        <div className='footer__data'>
+          <div className='contact__information'>
+            <span> Contact info</span>
+            <span>Address Line 1</span>
+            <span>Address Line 2</span>
+            <span>Address Line 3</span>
+            <span>Contact number</span>
+            <span>Email Address</span>
+          </div>
+          <div className='site__navigation'>
+            <a href='#'>Home</a>
+            <a href='#'>Gallery</a>
+            <a href='#'>Design</a>
+          </div>
+          <div className='social__media'>
+            <span>twitter</span>
+            <span>instagram</span>
+            <span>tic-tok</span>
+          </div>
         </div>
-        <div className="site__navigation">
-            <a href="#">Home</a>
-            <a href="#">Gallery</a>
-            <a href="#">Design</a>
-        </div>
-        <div className="social__media">
-            <p>twitter</p>
-            <p>instagram</p>
-        </div>
-    </footer>
+      </footer>
     </>
   );
 }

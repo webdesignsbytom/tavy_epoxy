e33b4ca2672354db86dd336fc94d61744f04c226
xtombrock <xtombrock1989@gmail.com>
2023-01-31 07:43:57 +0000

    about init

--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Nav from '../../components/nav/Nav'
+import Footer from '../../components/footer/Footer'
+import './about.css'
 
 function About() {
   return (
     <>
     <Nav />
-    <div>About</div>
+    <div className='about__page__container'>
+      <section className='about__sections'>
+        <div className="left"></div>
+        <div className="middle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio temporibus, veritatis facilis eligendi obcaecati veniam cupiditate iste praesentium pariatur, quo assumenda sed quibusdam sunt numquam dolor amet! Accusamus, alias rem! Cupiditate laudantium, accusantium eligendi esse beatae harum similique, dicta natus nam soluta hic autem. Quae excepturi repellendus vero maiores eos?</div>
+        <div className="right"></div>
+      </section>
+
+
+    </div>
+    <Footer />
     </>
   )
 }

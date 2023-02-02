a89e87a465b15c7270e2220fc5c4ee8c840651b3
Thomas Brockington <xtombrock1989@gmail.com>
2023-02-02 03:06:07 +0000

    save

--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import Nav from '../../components/nav/Nav'
+import React from 'react';
+import './about.css';
+import BG1 from '../../assets/images/bg1.jpg';
+import Nav from '../../components/nav/Nav';
 
 function About() {
   return (
     <>
-    <Nav />
-    <div>About</div>
+      <Nav />
+      <section className='about__page__container'>
+        <div className='about__info__container'>
+          <div className='top__layer'></div>
+          <div className='bottom__layer'>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default About
+export default About;

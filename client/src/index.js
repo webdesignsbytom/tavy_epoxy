b4d74ef39730a6470eeff31be7db3a3bc2283303
Thomas Brockington <xtombrock1989@gmail.com>
2023-04-28 08:57:17 +0100

    tailwind init

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+// Context
 import UserContextProvider from './context/UserContext';
 import ToggleContextProvider from './context/ToggleContext';
+// Styles
+import './index.css';
+import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
